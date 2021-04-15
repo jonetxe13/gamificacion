3450b02c98a0e95d4dae76b307e0926e71b12b17
jonetxe13 <jonetxeba.je10@gmail.com>
2021-04-15 08:30:11 +0200

    hola

--- logica.py
+++ logica.py
@@ -2,57 +2,57 @@ import random
 import pygame
 import numpy as np
 
-# pygame.init()
+pygame.init()
 
 GameState = True
 
 black = ( 0, 0, 0)
 
-screenw = 600
-screenh = 750
-# screen = pygame.display.set_mode((screenw,screenh))
+pos = 0
 
-tablero = ([0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0])
+# screenw = 600
+# screenh = 750
+# # screen = pygame.display.set_mode((screenw,screenh))
 
-posX = 0
-posY = 4
+tablero = ([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 
-dado = random.randint(1,6)
-# def pieza():
-#     for i in range(0, 6):
-#         for j in range(0, 5):
-#             if tablero[j][i] == 1:
-#                 pygame.draw.circle(screen, black, (50, 550), 25)
-print (dado)
+preg = (['1', '2', '3', '4', '5', '6', '7', '8'])
+
+dado = 0
+
+tablero[pos] = 0
 
 
-def mov():
-    global posX
-    global posY
-    if (posX + dado) <= 5:
-        tablero[posY][posX] = 0
-        tablero[posY][posX+dado] = 1
-        posX += dado
-    elif (posX + dado) >= 6:
-        tablero[posY][posX] = 0
-        posY -= 1
-        if posY % 2 != 0:
-            tablero[posY][6-((posX+dado)-5)] = 1
     
-    GameState == False
-        
-while GameState:
-    mov()
-    # if pygame.mouse.get_pressed() == True:
-    #     mov()
-    print(posX, posY)
-    print(tablero)
+print(pos)
 
-    #tablero = pygame.image.load("tablero.jpg")
-    #bg = pygame.transform.scale(tablero, (screenw, screenh))
-    #screen.blit(bg,(0,0))
-    #pygame.display.update()
+pos += dado
+print(dado)
+
+
+while GameState:
+    dado = random.randint(1, 6)
+
+    for i in range(len(tablero)):
+        if tablero[i] == 1:
+            print("preg")
+            print(preg[i+1])
+
+    for i in range(5):
+        tablero[pos+dado] = 1
+        pos += dado
+        print(tablero)
+    
+    # for i in range(len(tablero)):
+    #     if tablero[i] == 1:
+    #         print("preg")
+    #         print(preg[i+1])
+    
+    while dado != 9:
+        print("precione enter para lanzar dado")
+        input()
+        print(dado)
+    
+    print(pos)
+
+    GameState != GameState
